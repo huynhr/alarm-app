22cc27cf347f04686adf0ab6991c174631f353b5
Ray Huynh <huynh.r.k@gmail.com>
2020-02-29 13:47:28 -0700

    remove node logger because it's depreciated

--- database/controllers.js
+++ database/controllers.js
@@ -1,13 +1,11 @@
 const knex = require('./knex');
-const logger = require('../node_modules/node-logger').createLogger();
-logger.setLevel("debug");
 
 const createFindUser = (profile, callback) => {
-  logger.info('Creating or finding user...');
+  console.log('Creating or finding user...');
   knex.from('usersTable').where({profile_id: profile.id})
     .then(result => {
       if (result.length === 0) {
-        logger.debug('User not found, creating user...');
+        console.log('User not found, creating user...');
         date = knex.raw('CURRENT_TIMESTAMP')
         return knex('usersTable').insert({
           profile_id: profile.id,
@@ -16,13 +14,13 @@ const createFindUser = (profile, callback) => {
           updated_at: date
         }).then(user => {
           if (user) {
-            logger.debug('User created, fetching user...')
+            console.log('User created, fetching user...')
             knex.from('usersTable').where({profile_id: profile.id})
               .then(result => callback(result[0]))
           }
         });
       } else {
-        logger.debug('Found user...')
+        console.log('Found user...')
         return callback(result[0]);
       }
     });
