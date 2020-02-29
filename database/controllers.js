03d600a707ecb25bafe3eb188c9597f564590409
Ray Huynh <huynh.r.k@gmail.com>
2020-02-29 13:43:25 -0700

    Add logging

--- database/controllers.js
+++ database/controllers.js
@@ -1,20 +1,29 @@
 const knex = require('./knex');
+const logger = require('../node_modules/node-logger').createLogger();
+logger.setLevel("debug");
 
-const createFindUser = (profile) => {
+const createFindUser = (profile, callback) => {
+  logger.info('Creating or finding user...');
   knex.from('usersTable').where({profile_id: profile.id})
     .then(result => {
-      console.log('user: ', result)
       if (result.length === 0) {
-          date = new Date().toISOString();
-          date = knex.raw('CURRENT_TIMESTAMP')
-          return knex('usersTable').insert({
-            profile_id: profile.id,
-            name: profile.displayName,
-            created_at: date,
-            updated_at: date
-          }).then(user => user);
+        logger.debug('User not found, creating user...');
+        date = knex.raw('CURRENT_TIMESTAMP')
+        return knex('usersTable').insert({
+          profile_id: profile.id,
+          name: profile.displayName,
+          created_at: date,
+          updated_at: date
+        }).then(user => {
+          if (user) {
+            logger.debug('User created, fetching user...')
+            knex.from('usersTable').where({profile_id: profile.id})
+              .then(result => callback(result[0]))
+          }
+        });
       } else {
-        return result[0];
+        logger.debug('Found user...')
+        return callback(result[0]);
       }
     });
 };
--- server/passport.js
+++ server/passport.js
@@ -2,18 +2,6 @@ const passport = require('passport');
 const GoogleTokenStrategy = require('passport-google-token').Strategy;
 const { createFindUser } = require('../database/controllers');
 
-const _USERS = {}
-
-const saveUser = (accessToken, refreshToken, profile, cb) => {
-  _USERS[profile["id"]] = { accessToken, refreshToken, profile };
-  error = null;
-  let data = {
-      id: profile["id"],
-      ...profile
-  };
-  return cb(error, data);
-}
-
 module.exports = function() { 
   passport.use(new GoogleTokenStrategy(
       {
@@ -21,10 +9,9 @@ module.exports = function() {
         clientSecret: process.env.GOOGLE_SECRET
       },
       function(accessToken, refreshToken, profile, done) {
-        saveUser(accessToken, refreshToken, profile, (err, user) => {
-          createFindUser(profile)
-          return done(err, user);
-        })
+        createFindUser(profile, user => {
+          return done(null, user)
+        });
       }
     )
   );
